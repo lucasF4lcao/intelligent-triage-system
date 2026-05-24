47035dd38694fd354398bc1bcb8e10f927123442
Lucas Falcão <lucas.olifalco@gmail.com>
2026-05-24 02:53:29 -0300

    simplificando

--- api/triageai/src/main/java/com/triageai/api/controller/AnaliseTriagemController.java
+++ api/triageai/src/main/java/com/triageai/api/controller/AnaliseTriagemController.java
@@ -1,29 +1,18 @@
 package com.triageai.api.controller;
 
-import com.triageai.api.dto.request.CriarAnaliseRequest;
 import com.triageai.api.dto.response.AnaliseTriagemResponse;
 import com.triageai.api.service.AnaliseOrchestratorService;
-import com.triageai.api.service.AnaliseTriagemService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/analises")
 public class AnaliseTriagemController {
 
-    private final AnaliseTriagemService analiseTriagemService;
     private final AnaliseOrchestratorService orchestratorService;
 
-    public AnaliseTriagemController(AnaliseTriagemService analiseTriagemService, AnaliseOrchestratorService orchestratorService) {
-        this.analiseTriagemService = analiseTriagemService;
+    public AnaliseTriagemController(AnaliseOrchestratorService orchestratorService) {
         this.orchestratorService = orchestratorService;
-    }
-
-    @PostMapping
-    public ResponseEntity<AnaliseTriagemResponse> criarAnalise(@RequestBody CriarAnaliseRequest request) {
-        return ResponseEntity.ok(analiseTriagemService.criarAnalise(request));
     }
 
     @PostMapping("/fhir")
@@ -31,13 +20,4 @@ public class AnaliseTriagemController {
         return ResponseEntity.ok(orchestratorService.processarFhir(bundleJson));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<AnaliseTriagemResponse> buscarPorId(@PathVariable Long id) {
-        return ResponseEntity.ok(analiseTriagemService.buscarPorId(id));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<AnaliseTriagemResponse>> listarAnalises() {
-        return ResponseEntity.ok(analiseTriagemService.listarAnalises());
-    }
 }
